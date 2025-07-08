ae3d724d59df297eabaf8ce58d34f23c2f33487c
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-08 11:34:54 +0200

    Kontenery sekwencyjne 0x08 Tablica
    Zadanie #2

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
+// Zadanie #2
+
+//     Znajdź dokumentację std::array<T, N> na cppreference.com
+//     Stwórz nowy plik i napisz funkcję main()
+//     Stwórz std::array przechowujący wartości całkowitoliczbowe o rozmiarze 10
+//     Wypełnij ją wartościami 5
+//     Do czwartego elementu przypisz wartość 3
+//     Stwórz inną tablicę o tym samym rozmiarze
+//     Podmień tablice
+//     Wypisz obie tablice, każdą w osobnej linii
+
 #include <array>
 #include <iostream>
- 
 int main()
 {
-    std::array<int, 4> numbers{3, 1, 4, 1};
-    std::array<int, 0> no_numbers;
-    std::array<int, 3> numbers2;
- 
-    std::cout << std::boolalpha;
-    std::cout << "numbers.empty(): " << numbers.empty() << '\n';
-    std::cout << "no_numbers.empty(): " << no_numbers.empty() << '\n';
-    std::cout << "numbers2.empty(): " << numbers2.empty() << '\n';
-    for (auto i : numbers2) {
-      std::cout<<i<<", ";
-    
-    }
-    std::cout<<"\n"; //clang give initialized elements: 0, gcc not
+  std::array<int, 10> array;
+  array.fill(5);
+  array[3]=3;
+  std::array<int, 10> array2;
+  array.swap(array);
+  for (auto i : array) {
+    std::cout<<i<<", ";
+  
+  }
+  std::cout<<"\n";
+  for (auto i : array2) {
+    std::cout<<i<<", ";
+  
+  }
+  std::cout<<"\n";
 }
