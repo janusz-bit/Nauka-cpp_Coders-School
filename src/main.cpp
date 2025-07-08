44f890b36fcd66512c8b5e34bc4df44aa1d955ba
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-08 09:40:37 +0200

    Kontenery sekwencyjne 0x06 Wektor
    ZADANIE 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,32 @@
-// Zadanie #1
+// Zadanie #2
 
 //     Otwórz dokumentację wektora na cppreference.com
 //     Stwórz nowy plik i napisz funkcję main()
-//     Stwórz wektor o wartościach {1, 2, 4, 5, 6}
-//     Usuń pierwszą wartość
-//     dodaj wartość 5 na końcu wektora
-//     dodaj wartość 12 na początku wektora korzystając z metody emplace()
-//     Wypisz rozmiar wektora i maksymalny możliwy rozmiar
-//     Wypisz zawartość wektora
-//     Wyczyść wektor
-//     Wypisz rozmiar wektora
+//     Stwórz pusty wektor
+//     Wypisz rozmiar i pojemność wektora
+//     Zmień rozmiar wektora na 10 i wypełnij go wartościami 5
+//     Wypisz rozmiar i pojemność wektora
+//     Zarezerwuj pamięć na 20 elementów
+//     Wypisz rozmiar i pojemność wektora
+//     Zredukuj pojemność wektora metodą shrink_to_fit()
+//     Wypisz rozmiar i pojemność wektora
 
 #include <iostream>
 #include <vector>
 
-int main() {
-  std::vector<int> vec{1, 2, 4, 5, 6};
-  vec.erase(vec.begin());
-  vec.push_back(5);
-  vec.emplace(vec.begin(), 12);
-  std::cout << "vec.size(): " << vec.size() << '\n';
-  std::cout << "vec.max_size(): " << vec.max_size() << '\n';
-  for (auto i : vec) {
-    std::cout<<i<<", ";
-  }
-  std::cout<<'\n';
+void readVec(std::vector<int> &vec) {
+  std::cout << "vec.size(), vec.capacity(): " << vec.size() << ", "
+            << vec.capacity() << '\n';
+}
 
-  vec.clear();
-  std::cout << "vec.size(): " << vec.size() << '\n';
+int main() {
+  std::vector<int> vec;
+  readVec(vec);
+  vec.resize(10, 5);
+  readVec(vec);
+  vec.reserve(20);
+  readVec(vec);
+  vec.shrink_to_fit();
+  readVec(vec);
+
 }
