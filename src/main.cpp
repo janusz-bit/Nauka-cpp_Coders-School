f42ddfdac6e508dc8ae5d515d419e62f05a4ab23
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-08 11:03:55 +0200

    Kontenery sekwencyjne 0x08 Tablica
    Zadanie #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,19 @@
-// Zadanie #2
-
-//     Otwórz dokumentację wektora na cppreference.com
-//     Stwórz nowy plik i napisz funkcję main()
-//     Stwórz pusty wektor
-//     Wypisz rozmiar i pojemność wektora
-//     Zmień rozmiar wektora na 10 i wypełnij go wartościami 5
-//     Wypisz rozmiar i pojemność wektora
-//     Zarezerwuj pamięć na 20 elementów
-//     Wypisz rozmiar i pojemność wektora
-//     Zredukuj pojemność wektora metodą shrink_to_fit()
-//     Wypisz rozmiar i pojemność wektora
-
+#include <array>
 #include <iostream>
-#include <vector>
-
-void readVec(std::vector<int> &vec) {
-  std::cout << "vec.size(), vec.capacity(): " << vec.size() << ", "
-            << vec.capacity() << '\n';
-}
-
-int main() {
-  std::vector<int> vec;
-  readVec(vec);
-  vec.resize(10, 5);
-  readVec(vec);
-  vec.reserve(20);
-  readVec(vec);
-  vec.shrink_to_fit();
-  readVec(vec);
-
+ 
+int main()
+{
+    std::array<int, 4> numbers{3, 1, 4, 1};
+    std::array<int, 0> no_numbers;
+    std::array<int, 3> numbers2;
+ 
+    std::cout << std::boolalpha;
+    std::cout << "numbers.empty(): " << numbers.empty() << '\n';
+    std::cout << "no_numbers.empty(): " << no_numbers.empty() << '\n';
+    std::cout << "numbers2.empty(): " << numbers2.empty() << '\n';
+    for (auto i : numbers2) {
+      std::cout<<i<<", ";
+    
+    }
+    std::cout<<"\n"; //clang give initialized elements: 0, gcc not
 }
