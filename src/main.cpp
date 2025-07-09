54f2ab81c5d1a302d510a34d4c54b51d0cb1a0ce
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-09 20:26:46 +0200

    Kontenery sekwencyjne 0x0E Kolejka dwukierunkowa

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,35 @@
 
-// Znajdź dokumentację std::forward_list na cppreference.com
-// Skorzystaj z kodu z zadania std::list
-// Stwórz listę jednokierunkową zawierającą elementy od 0 do 6
-// Wyświetl listę
-// Usuń trzeci element z listy
-// Dodaj na początek i na koniec listy wartość 10
-// Wyświetl listę
+// Znajdź dokumentację std:deque na cppreference.com
+// Stwórz nowy plik i napisz funkcję main()
+// Stwórz pusty deque
+// Dodaj do niego pięć dowolnych wartości
+// Wyświetl deque
+// Usuń drugi i czwarty element
+// Dodaj na początek i koniec wartość 30
+// Wyświetl deque
 // Dodaj na czwartej pozycji liczbę 20
-// Wyświetl listę
+// Wyświetl deque
 
-#include <forward_list>
+#include <deque>
 #include <iostream>
-#include <iterator>
 int main() {
-  std::forward_list<int> flist{0, 1, 2, 3, 4, 5, 6};
-  for (auto it : flist) {
-    std::cout << it<<", ";
+  std::deque<int> deque;
+  deque = {1,2,3,4,5};
+  for (auto it : deque) {
+    std::cout<<it<<", ";
   }
   std::cout<<'\n';
-  auto it = flist.begin();
-  std::advance(it,1);
-  flist.erase_after(it);
-  flist.push_front(10);
-
-  it = flist.begin();
-  auto itNext = it;
-  ++itNext;
-  while (itNext != flist.end()) {
-    ++it;
-    ++itNext;
+  deque.erase(deque.begin()+1);
+  deque.erase(deque.begin()+2);
+  deque.insert(deque.begin(),30);
+  deque.insert(deque.end(),30);
+  for (auto it : deque) {
+    std::cout<<it<<", ";
   }
-  flist.emplace_after(it, 10);
-  
-  
-  for (auto it : flist) {
-    std::cout << it<<", ";
+  std::cout<<'\n';
+  deque.insert(deque.begin()+3,20);
+  for (auto it : deque) {
+    std::cout<<it<<", ";
   }
-  std::cout<<"\n";
-
-  it = flist.begin();
-  std::advance(it,2);
-  flist.insert_after(it,20);
-  for (auto it : flist) {
-    std::cout << it<<", ";
-  }
-  
+  std::cout<<'\n';
 }
