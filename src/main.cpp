ead11db1eff354908d6901aac7c1db7b453463b2
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-11 17:02:34 +0200

    Kontenery sekwencyjne 0x12 Zadania domowe
    Zadanie 3a - compressGrayscale()

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,71 @@
-// ## Zadanie 2 - `length-sort`
+// Napisz funkcję `compressGrayscale()`.
+// Powinna ona przyjąć jeden argument typu `std::array<std::array<uint8_t, 240>,
+// 160>` określający rozkład odcieni szarości na obrazku 2D (który w dalszej
+// części nazywać będziemy bitmapą) i zwróci `std::vector<std::pair<uint8_t,
+// uint8_t>>` zawierający skompresowaną bitmapę.
 
-// Napisz funkcję `lengthSort()`.
+// Kompresja powinna przebiegać w następujący sposób:
 
-// Ma ona przyjąć `std::forward_list<std::string>` i zwrócić
-// `std::deque<std::string>` z posortowanymi słowami od najkrótszego do
-// najdłuższego. Jeżeli dwa lub więcej słów ma tyle samo znaków posortuj je
-// leksykograficznie.
+// * Bitmapę rysujemy od górnego lewego rogu przechodząc w prawo, następnie
+// poziom niżej.
+// * Jeżeli obok siebie występuje ten sam kolor więcej niż 1 raz, funkcja
+// powinna wrzucić do  `std::vector` wartość tego koloru (liczba z przedziału 0
+// – 255) jako pierwszy element pary oraz ilość jego powtórzeń jako drugi
+// element pary.
+// * Jeżeli obok siebie występują różne odcienie to funkcja powinna wypełnić
+// `std::vector` wartością odcienia oraz liczbą wystąpień równą 1 (w tym
+// przypadku pogarszamy optymalizację, gdyż przechowujemy 2x tyle danych, jednak
+// najczęściej te same kolory są położone obok siebie).
 
-// * Input: `std::forward_list<std::string>{"Three", "One", "Four", "Two"}`
-// * Output: `std::deque<std::string>{"One", "Two", "Four", "Three"}`
-
-#include <algorithm>
-#include <deque>
-#include <forward_list>
-#include <functional>
+#include <array>
+#include <cstdint>
 #include <iostream>
-#include <string>
+#include <utility>
+#include <vector>
 
-std::deque<std::string>
-lengthSort(const std::forward_list<std::string> &input) {
-  std::deque<std::string> output;
-  for (const auto& String: input) {
-    output.push_back(String);
+std::vector<std::pair<uint8_t, uint8_t>>
+compressGrayscale(std::array<std::array<uint8_t, 10>, 3> input) {
+  std::vector<std::pair<uint8_t, uint8_t>> output;
+
+  uint8_t counter{};
+  uint8_t color = input[0][0];
+
+  for (const auto &row : input) {
+    for (const auto &i : row) {
+      if (color == i) {
+        ++counter;
+
+      } else {
+        output.push_back(std::make_pair(color, counter));
+        counter = 1;
+        color = i;
+      }
+    }
   }
-  std::sort(output.begin(),output.end(),[](std::string a, std::string b){
-    if (a.length() == b.length()) {
-      return std::less<std::string>{}(a,b);
-    }
-    else 
-    {
-      return a.length()<b.length();
-    }
-  });
+  output.push_back(std::make_pair(color, counter));
+
   return output;
 }
 
-int main()
-{
-  std::forward_list<std::string> input{"Three", "One", "Four", "Two"};
-  std::deque<std::string> output = lengthSort(input);
+int main() {
+  std::array<std::array<uint8_t, 10>, 3> input = {
+      {{{0, 0, 0, 1, 1, 2, 3, 0, 0, 0}},
+       {{0, 0, 4, 4, 4, 1, 1, 1, 1, 1}},
+       {{2, 2, 2, 2, 2, 1, 2, 2, 2, 2}}
+
+      }};
+
+  auto output = compressGrayscale(input);
 
   std::cout<<"{";
-  bool isFirst = true;
-  for (auto i : output) {
-    
-    if(isFirst)
-    {
-      isFirst = false;
+  bool isFirst{1};
+  for (auto pair : output) {
+    if (isFirst) {
+      isFirst = 0;
+    } else {
+      std::cout << ", ";
     }
-    else {
-      std::cout<<", ";
-    }
-    std::cout<<"\""<<i<<"\"";
+    std::cout<<"{"<<static_cast<int>(pair.first)<<", "<<static_cast<int>(pair.second)<<"}";
   }
-  std::cout<<"}\n";
+  std::cout<<"}";
 }
