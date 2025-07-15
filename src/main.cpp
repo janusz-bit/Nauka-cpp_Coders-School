a63e7fd435a79a69ff6395f604ff14f8629b841d
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-15 09:19:25 +0200

    Algorytmy 0x07 Przeszukiwanie

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,60 @@
 
-// Utwórz std::vector<int> v = {8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9};
-// Policz, ile jest elementów większych lub równych 5
-// Sprawdź, czy istnieje element mniejszy od 1
-// Sprawdź, czy wszystkie elementy są większe od 1
+// Utwórz std::vector<int> v = {8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9}; (ten sam
+// co ostatnio) Znajdź wszystkie elementy większe od 6 i wypisz je Znajdź
+// wszystkie elementy równe 2, 4, 6 lub 8 i wypisz je Przeszukaj wektor v w
+// poszukiwaniu zakresów {6, 6} i {7, 7} Użyj std::adjacent_find na wektorze v
 
 #include <algorithm>
 #include <iostream>
 #include <vector>
 int main() {
   std::vector<int> v = {8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9};
-  std::cout<<std::ranges::count_if(v, [](int i) {
-    if (i >= 5) {
-      return true;
+
+  auto begin = v.begin();
+  while (1) {
+    begin = std::find_if(begin, v.end(), [](int i) {
+      if (i > 6) {
+        return true;
+      }
+      return false;
+    });
+    if (begin == v.end()) {
+      break;
+    } else {
+      std::cout << *begin << '\n';
+      begin++;
     }
-    return false;
-  })<<"\n";
-  std::cout<<std::ranges::any_of(v,[](int i){
-    return i<1;
-  })<<'\n';
-  std::cout<<std::ranges::all_of(v,[](int i){return i>1;})<<'\n';
+  }
+
+  std::cout << "---\n";
+
+  std::vector<int> Find = {2, 4, 6, 8};
+
+  auto begin1 = v.begin();
+  while (1) {
+    begin1 = std::find_first_of(begin1, v.end(), Find.begin(), Find.end());
+    if (begin1 == v.end()) {
+      break;
+    }
+    std::cout << *begin1 << '\n';
+    begin1++;
+  }
+  std::cout << "---\n";
+
+  std::vector<std::vector<int>> search = {{6, 6}, {7, 7}};
+
+  for (auto it : search) {
+
+    auto begin2 = v.begin();
+    while (1) {
+      begin2 = std::search(begin2, v.end(), it.begin(), it.end());
+      if (begin2 == v.end()) {
+        break;
+      }
+      std::cout << begin2 - v.begin() << '\n';
+      begin2++;
+    }
+  }
+
+  return 0;
 }
