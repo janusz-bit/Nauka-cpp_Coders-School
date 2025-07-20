22259d704ed228347ce9cec6fc07c6043deae10c
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-20 22:32:18 +0200

    Kontenery Asocjacyjne 0x08 set i multiset

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,60 @@
-// Zadanie
-// struct Point {
-//   int x;
-//   int y;
-// }
-// Struktura Point ma reprezentować punkt o współrzędnych x i y na mapie. Mamy
-// też dane 4 miast:
-
-// Wrocław (x = 17, y = 51)
-// Moskwa (x = 37, y = 55)
-// Nowy Jork (x = -74, y = 40)
-// Sydney (x = 151, y = -33)
-// Część A - łatwiejsza
-// Stwórz std::map<std::string, Point>, która będzie przechowywać powyższe
-// miejsca Sprawdź czy w mapie jest element, który znajduje się w promieniu 70
-// od środka układu współrzędnych (0, 0) Pobierz i wypisz współrzędne Sydney
-// Zadanie
-// struct Point {
-//   int x;
-//   int y;
-// }
-// Struktura Point ma reprezentować punkt o współrzędnych x i y na mapie. Mamy
-// też dane 4 miast:
-
-// Wrocław (x = 17, y = 51)
-// Moskwa (x = 37, y = 55)
-// Nowy Jork (x = -74, y = 40)
-// Sydney (x = 151, y = -33)
-// Część B - trudniejsza
-// Skopiuj te dane do mapy std::map<Point, std::string>
-// Sprawdź czy w odwróconej mapie jest element, który znajduje się w promieniu
-// 70 od środka układu współrzędnych (0, 0) Pobierz i wypisz współrzędne Sydney
-
-#include <cmath>
+#include <algorithm>
+#include <cstddef>
+#include <functional>
 #include <iostream>
-#include <map>
-#include <ostream>
-#include <string>
+#include <iterator>
+#include <ranges>
+#include <set>
 
-struct Point;
-
-float PointDistance(Point point1);
-
-struct Point {
-  int x;
-  int y;
-
-  bool operator<(const Point &input) const {
-    return PointDistance(*this) < PointDistance(input);
+template <typename Container>
+void print(const Container &container, int newLine = 1) {
+  std::copy(
+      container.begin(), container.end(),
+      std::ostream_iterator<typename Container::value_type>(std::cout, "; "));
+  if (newLine) {
+    for (size_t i{}; i < newLine; ++i) {
+      std::cout << '\n';
+    }
   }
-};
-
-std::ostream& operator<<(std::ostream& stream, const Point& point)
-{
-  stream<<"{"<<point.x<<"; "<<point.y<<"}";
-  return stream;
-}
-
-float PointDistance(Point point1) {
-  const int x = point1.x;
-  const int y = point1.y;
-  return std::sqrt(x * x + y * y);
 }
 
 int main() {
+  std::set<int, std::greater<int>> myMap;
 
-  std::map<std::string, Point> myMap = {{"Wrocław", {17, 51}},
-                                        {"Moskwa", {37, 55}},
-                                        {"Nowy Jork", {-74, 40}},
-                                        {"Sydney", {151, -33}}};
-  for (auto myPoint : myMap) {
-    if (((myPoint.second.x * myPoint.second.x) +
-         (myPoint.second.y * myPoint.second.y)) < (70 * 70)) {
-      std::cout << myPoint.first << '\n';
+  auto number = std::views::iota(-20, 21);
+  for (auto it : number) {
+    myMap.insert(it);
+  }
+
+  myMap.insert({-10, 0, 10, 100, -100});
+
+  print(myMap, 2);
+
+  auto number2 = std::views::iota(0, 40);
+
+  std::multiset<int> myMultiMap;
+  for (auto it : number2) {
+    myMultiMap.insert(it);
+  }
+  myMultiMap.insert({-10, 0, 10, 100, -100});
+
+  print(myMultiMap, 2);
+
+  std::multiset<int> endContainer;
+
+  for (auto it : myMap) {
+    endContainer.insert(it);
+  }
+
+  for (auto it : myMultiMap) {
+    endContainer.insert(it);
+  }
+
+  print(endContainer,2);
+
+  for (auto it : endContainer) {
+    if (it == 0 || it ==50) {
+      std::cout<<it<<"; ";
     }
   }
-
-  std::map<Point, std::string> myMap2({{{17, 51}, "Wrocław"},
-                                       {{37, 55}, "Moskwa"},
-                                       {{-74, 40}, "Nowy Jork"},
-                                       {{151, -33}, "Sydney"}});
-
-
-  for (auto it = myMap2.begin(); PointDistance(it->first) < 70 && it != myMap2.end(); ++it) {
-    std::cout<<it->second<<": "<<it->first<<'\n';
-  }
-
 }
