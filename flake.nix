848407b27dced210601c3bbfac0bf65b38c4651e
fatmichealcoder <fatmichealcoder@gmail.com>
2025-07-24 02:07:16 +0200

    Update flake.lock and flake.nix for SDL project environment enhancements

--- flake.nix
+++ flake.nix
@@ -17,24 +17,60 @@
         system = linux;
         config.allowUnfree = true;
       };
+      pkgsCross = import nixpkgs {
+        system = linux;
+        config.allowUnfree = true;
+        crossSystem = {
+          config = "x86_64-w64-mingw32";
+          libc = "msvcrt"; # This distinguishes the mingw (non posix) toolchain
+        };
+      };
+
     in
     {
-      devShells.${linux}.default = pkgs.mkShell {
+      devShells.${linux} = {
+        default = pkgs.mkShell {
 
-        name = "sdl-dev-shell";
-        shellHook = ''
-          echo "Welcome to the development environment!"
-        '';
-        buildInputs = with pkgs; [
-          cmake
-          pkg-config
-          gcc
-          clang-tools
-          gnumake
-          ninja
-          sdl3
-          gdb
-        ];
+          name = "sdl-dev-shell";
+          shellHook = ''
+            echo "Welcome to the development environment!"
+          '';
+          buildInputs = with pkgs; [
+            cmake
+            pkg-config
+            gcc
+            clang-tools
+            # gnumake
+            ninja
+            sdl3
+            gdb
+          ];
+        };
+        windows = pkgsCross.mkShell {
+
+          name = "sdl-dev-shell";
+          shellHook = ''
+            echo "Welcome to the development environment!"
+            HOME=$PWD/replacement-home
+            echo "${pkgsCross.stdenv.hostPlatform.emulator pkgsCross.buildPackages}"
+          '';
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+            gcc
+            clang-tools
+            # gnumake
+            ninja
+            wine
+            wine64
+            winetricks
+            gdb
+          ];
+
+          buildInputs = with pkgsCross; [
+            sdl3
+          ];
+        };
       };
 
     };
